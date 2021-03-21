f6beed0af55d760f1ab0eb0ea119863c97e7ca75
Guilherme Rodrigues <guilherme.imlerith@gmail.com>
2021-03-20 22:51:21 -0300

    Remove List Specifications use case

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,16 +1,11 @@
 import { Router } from "express";
 
 import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
-import { listSpecificationsController } from "../modules/cars/useCases/listSpecifications";
 
 const specificationsRouter = Router();
 
 specificationsRouter.post("/", (req, res) => {
   createSpecificationController.handle(req, res);
-});
-
-specificationsRouter.get("/", (req, res) => {
-  listSpecificationsController.handler(req, res);
 });
 
 export { specificationsRouter };
